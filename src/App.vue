c5005248ecf148a7a9e87a13bc57de155fad7511
Alexander <milovanska@gmail.com>
2022-11-27 21:29:36 +0300

    feat: create repo

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <MainLayout>
+        <RouterView />
+    </MainLayout>
+</template>
+
+<script setup>
+import { MainLayout } from './layouts';
+import { RouterView } from 'vue-router';
+</script>
+
+<style scoped></style>

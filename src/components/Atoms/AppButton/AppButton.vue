e02f7cb9dcc6480a41e71b1f2ef317ca753b3757
Alexander <milovanska@gmail.com>
2022-11-27 22:16:57 +0300

    feat: add AppButton

---
+++ src/components/Atoms/AppButton/AppButton.vue
@@ -0,0 +1,35 @@
+<template>
+    <button :class="['app-button', { primary, secondary }, size]">
+        <span><slot /></span>
+        <svg v-if="loading">
+            <use xlink:href="/sprite.svg#loader"></use>
+        </svg>
+    </button>
+</template>
+
+<script setup>
+import './index.scss';
+
+defineProps({
+    loading: {
+        type: Boolean
+    },
+    primary: {
+        type: Boolean
+    },
+    secondary: {
+        type: Boolean
+    },
+    size: {
+        type: String,
+        default: '',
+        validator: function (value) {
+            return (
+                ['', 'medium', 'large', 'block', 'small'].indexOf(value) !== -1
+            );
+        }
+    }
+});
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/Atoms/index.js
@@ -0,0 +1,3 @@
+import AppButton from './AppButton/AppButton.vue';
+
+export { AppButton };
